60f64a4b9280dfb22e2cacfae28cb37161699802
saurav_kalsoor <saurav.kalsoor@gmail.com>
2021-08-10 12:15:23 +0530

    task 10 code review

--- app/src/main/java/com/getdefault/task9/DisplayFragment.kt
+++ app/src/main/java/com/getdefault/task9/DisplayFragment.kt
@@ -1,5 +1,7 @@
 package com.getdefault.task9
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.getdefault.task9.databinding.FragmentDisplayBinding
+
 
 class DisplayFragment : Fragment() {
 
@@ -19,9 +22,18 @@ class DisplayFragment : Fragment() {
         val viewModel: DisplayViewModel = ViewModelProvider(this).get(DisplayViewModel::class.java)
         binding.viewModel = viewModel
 
-        val adapter = PersonAdapter()
+
+        val adapter = PersonAdapter { phone: String -> callPerson(phone) }
+
         binding.rvPersonList.adapter = adapter
 
+
         return binding.root
+    }
+
+    private fun callPerson(phone : String){
+        val callIntent = Intent(Intent.ACTION_CALL)
+        callIntent.data = Uri.parse("tel:$phone")
+        startActivity(callIntent)
     }
 }
